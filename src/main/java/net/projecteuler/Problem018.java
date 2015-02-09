f6efeab43a828eee2eb03755a99cb7405bf037a0
Zsolt Thalmeiner <thaliga@gmail.com>
2015-02-09 22:21:19 +0100

    solution for 18 and 67

--- src/main/java/net/projecteuler/Problem018.java
+++ src/main/java/net/projecteuler/Problem018.java
@@ -1,13 +1,59 @@
-
 package net.projecteuler;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Problem018 {
 
-    public static long solution() {
-        return 0L;
-    }
+	private final ArrayList<Long> sumArray = new ArrayList<>();
 
-    public static long solutionBruteforce() {
-        return 0L;
-    }
+	public static long solution(InputStream inputStream) {
+		Problem018 p = new Problem018();
+		Scanner scanner = new Scanner(inputStream);
+		while (scanner.hasNextLine()) {
+			p.processLine(scanner.nextLine());
+		}
+		scanner.close();
+		return p.getResult();
+	}
+
+	ArrayList<Long> processLine(String nextLine) {
+		ArrayList<Long> numbers = convertToNumbers(nextLine);
+		if (numbers.size() != sumArray.size() + 1) {
+			throw new RuntimeException();
+		}
+		if (sumArray.size() == 0) {
+			sumArray.add(numbers.get(0));
+		} else {
+			sumArray.add(sumArray.get(sumArray.size() - 1)
+					+ numbers.get(numbers.size() - 1));
+			for (int i = sumArray.size() - 2; i > 0; --i) {
+				long a = sumArray.get(i - 1) + numbers.get(i);
+				long b = sumArray.get(i) + numbers.get(i);
+				sumArray.set(i, Math.max(a, b));
+			}
+			sumArray.set(0, sumArray.get(0) + numbers.get(0));
+		}
+		return sumArray;
+	}
+
+	private static ArrayList<Long> convertToNumbers(String line) {
+		ArrayList<Long> result = new ArrayList<Long>();
+		Scanner scanner = new Scanner(line);
+		while (scanner.hasNextLong()) {
+			result.add(scanner.nextLong());
+		}
+		scanner.close();
+		return result;
+	}
+
+	long getResult() {
+		return Collections.max(sumArray);
+	}
+
+	public static long solutionBruteforce(InputStream inputStream) {
+		return 0L;
+	}
 }
--- src/test/java/net/projecteuler/Problem018Test.java
+++ src/test/java/net/projecteuler/Problem018Test.java
@@ -5,6 +5,12 @@ import static net.projecteuler.Problem018.solution;
 import static net.projecteuler.Problem018.solutionBruteforce;
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class Problem018Test {
@@ -12,18 +18,102 @@ public class Problem018Test {
 	@Test
 	public void testSolution() throws Exception {
 		double time = nanoTime();
-		long result = solution();
+		InputStream inputStream = loadResource("Problem018.input");
+		long result = solution(inputStream);
 		System.out.println("[000] solution (" + result + ") in "
 				+ (nanoTime() - time) / 1000000 + " milliseconds.");
-		assertEquals(0L, result);
+		assertEquals(1074L, result);
 	}
 
 	@Test
 	public void testSolutionBruteforce() throws Exception {
 		double time = nanoTime();
-		long result = solutionBruteforce();
+		long result = solutionBruteforce(null);
 		System.out.println("[000] brute force solution (" + result + ") in "
 				+ (nanoTime() - time) / 1000000 + " milliseconds.");
 		assertEquals(0L, result);
+	}
+
+	public InputStream loadResource(String path) {
+		ClassLoader classLoader = getClass().getClassLoader();
+		try {
+			File file = new File(classLoader.getResource(path).getFile());
+			return new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Test
+	public void processLine_oneLine() throws Exception {
+		// given
+		Problem018 p = new Problem018();
+
+		// when
+		ArrayList<Long> sums = p.processLine("3");
+
+		// then
+		assertEquals(1, sums.size());
+		assertEquals(3L, (long) sums.get(0));
+		assertEquals(3L, p.getResult());
+	}
+
+	@Test
+	public void processLine_twoLines() throws Exception {
+		// given
+		Problem018 p = new Problem018();
+
+		// when
+		p.processLine("3");
+		ArrayList<Long> sums = p.processLine("7 4");
+
+		// then
+		assertEquals(2, sums.size());
+		assertEquals(10L, (long) sums.get(0));
+		assertEquals(7L, (long) sums.get(1));
+		assertEquals(10L, p.getResult());
+	}
+	
+	@Test
+	public void processLine_threeLines() throws Exception {
+		// given
+		Problem018 p = new Problem018();
+
+		// when
+		p.processLine("3");
+		p.processLine("7 4");
+		ArrayList<Long> sums = p.processLine("2 4 6");
+
+		// then
+		assertEquals(3, sums.size());
+		assertEquals(12L, (long) sums.get(0));
+		assertEquals(14L, (long) sums.get(1));
+		assertEquals(13L, (long) sums.get(2));
+		assertEquals(14L, p.getResult());
+	}
+
+	@Test
+	public void testSolution_mini() throws Exception {
+		// given
+		InputStream inputStream = loadResource("Problem018.mini.input");
+
+		// when
+		long result = solution(inputStream);
+		
+		// then
+		assertEquals(23L, result);
+	}
+
+	@Test
+	public void testSolution_67() throws Exception {
+		// given
+		InputStream inputStream = loadResource("Problem067.input");
+
+		// when
+		long result = solution(inputStream);
+		
+		// then
+		assertEquals(7273L, result);
 	}
 }
